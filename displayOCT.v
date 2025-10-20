ac999c487ef583c34ade7fae2134691bb90d6702
Walef <walef.emailexclusivo@gmail.com>
2025-10-20 19:30:35 -0300

    Refactor displayOCT and segment modules

--- displayOCT.v
+++ displayOCT.v
@@ -1,26 +1,25 @@
 /* - - - SEGMENTOS DO DISPLAY - - -*/
 
 // --- Segmento A ---
+
 module OCTA(OA, A0, A1, A2);
 
 	input A0, A1, A2;
 	output OA;
 	
 	wire nota0, nota1, nota2;
-	wire and0, and1, and2, and3;
+	wire and0, and1, and2;
 
 	// Inversores
 	not NotA0(nota0, A0);
 	not NotA1(nota1, A1);
 	not NotA2(nota2, A2);
 
-	// OA = ~A0~A1A2 + A0~A1~A2
-	and And0(and0, nota0, nota1);
-	and And1(and1, and0, A2);
-	and And2(and2, A0, nota1);
-	and And3(and3, and2, nota2);
+	// OA =  A0'A1'A2 + A0A1'A2'
+	and And0(and0, nota0, nota1, A2);
+	and And1(and1, A0, nota1, nota2);
 	
-	or OrFinal(OA, and1, and3);
+	or OrFinalA(OA, and0, and1);
 	
 endmodule
 
@@ -38,14 +37,13 @@ module OCTB(OB, A0, A1, A2);
 	not NotA0(nota0, A0);
 	not NotA1(nota1, A1);
 	not NotA2(nota2, A2);
-
-	// OB = A0~A1A2 + A0A1~A2
-	and And0(and0, A0, nota1);
-	and And1(and1, and0, A2);
-	and And2(and2, A0, A1);
-	and And3(and3, and2, nota2);
 	
-	or OrFinal(OB, and1, and3);
+	// OB = A0 A1'A2 + A0 A1 A2'
+
+	and And0(and0, A0, nota1, A2);
+	and And1(A0, A1, nota2);
+
+	or OrFinalB(OB, and0, and1);
 	
 endmodule
 
@@ -126,20 +124,19 @@ module OCTF(OF, A0, A1, A2);
 	output OF;
 	
 	wire nota0, nota1, nota2;
-	wire and0, and1, and2, or0;
+	wire and0, and1, and2;
 
 	// Inversores
 	not NotA0(nota0, A0);
 	not NotA1(nota1, A1);
 	not NotA2(nota2, A2);
-
-	// OF = ~A0A2 + ~A0A1 + A1A2
+	
+	//OF = A0'A2 + A0'A1 + A1A2
 	and And0(and0, nota0, A2);
 	and And1(and1, nota0, A1);
 	and And2(and2, A1, A2);
 	
-	or Or0(or0, and0, and1);
-	or OrFinal(OF, or0, and2);
+	or OrFinalF(OF, and0, and1, and2);
 	
 endmodule
 
@@ -163,7 +160,7 @@ module OCTG(OG, A0, A1, A2);
 	and And1(and1, A0, A1);
 	and And2(and2, and1, A2);
 	
-	or OrFinal(OG, and0, and2);
+	or OrFinalG(OG, and0, and2);
 	
 endmodule
 
@@ -184,3 +181,4 @@ module displayOCT (OCTSEG, A0, A1, A2);
 	OCTG SegG (OCTSEG[6], A0, A1, A2);
 
 endmodule
+
