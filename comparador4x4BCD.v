93301953ea7ce1f91af2d9e6ccbba76372b9e6e4
Walef <walef.emailexclusivo@gmail.com>
2025-10-20 21:31:26 -0300

    Refactor comparador4x4BCD to use a single input vector

--- comparador4x4BCD.v
+++ comparador4x4BCD.v
@@ -1,14 +1,14 @@
-module comparador4x4BCD(S, A3, A2, A1, A0);
+module comparador4x4BCD(S, A);
 
-  input A3, A2, A1, A0;
+  input [3:0] A;
   output S;
 
   wire and0, and1;
   //S = A2 A0 + A2 A1 + A3
 
-  and And0(and0, A2, A0);
-  and And1(and1, A2, A1);
+  and And0(and0, A[2], A[0]);
+  and And1(and1, A[2], A[1]);
 
-  or OrFinalS(S, and0, and1, A3);
+  or OrFinalS(S, and0, and1, A[3]);
 
 endmodule
